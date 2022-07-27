a51d959c6f233d96018f3004fdd5218658afe821
ravid <ravidrio25@gmail.com>
2022-07-27 12:33:21 +0530

    alignment

--- src/app/service-admin/components/breakdownservice-pdf/breakdownservice-pdf.component.ts
+++ src/app/service-admin/components/breakdownservice-pdf/breakdownservice-pdf.component.ts
@@ -16,12 +16,12 @@ import { SESSION_STORAGE, StorageService } from 'ngx-webstorage-service';
 })
 export class BreakdownservicePdfComponent implements OnInit {
 
-  rows:any;
+  rows: any;
 
-  storaged_data : any;
+  storaged_data: any;
 
-  
-  
+  logo = '../../../../assets/images/logo.png';
+
   searchQR;
 
   iso_number = '';
@@ -43,14 +43,14 @@ export class BreakdownservicePdfComponent implements OnInit {
   remarks = '';
   tech_signature = '';
   customer_signature = '';
-  signature_date  = '';
+  signature_date = '';
 
 
 
 
 
 
-  constructor(private router:Router, private toastr:ToastrManager,private _api: ApiService, @Inject(SESSION_STORAGE) private storage: StorageService) { }
+  constructor(private router: Router, private toastr: ToastrManager, private _api: ApiService, @Inject(SESSION_STORAGE) private storage: StorageService) { }
 
 
 
@@ -59,16 +59,46 @@ export class BreakdownservicePdfComponent implements OnInit {
     let job_detail = this.storage.get('job_detail');
     console.log(job_detail);
     let datas = {
-      job_id : job_detail.SMU_SCH_JOBNO
+      job_id: job_detail.SMU_SCH_JOBNO
     }
     this._api.breakdown_data_details(datas).subscribe((response: any) => {
       console.log(response.Data);
       this.storaged_data = response.Data;
+      this._api.fetch_breakdown_job_detail(datas).subscribe((response: any) => {
+        console.log(response.Data);
+        this.job_no = response.Data.SMU_SCH_JOBNO;
+        this.customer_name = response.Data.SMU_SCH_CUSNAME;
+        this.location = response.Data.SMU_SCH_CUSADD1 || '' + "," + response.Data.SMU_SCH_CUSADD2 || '' + "," + response.Data.SMU_SCH_CUSADD3 || '' + "," + response.Data.SMU_SCH_CUSADD4 || '' + "," + response.Data.SMU_SCH_CUSCODE || '';
+        this.date = response.Data.SMU_SCH_CRTDT;
+        this.nature_of_contract = response.Data.SMU_SCH_AMCTYPE;
+        this.breakdown_no = response.Data.SMU_SCH_COMPNO;
+        this.tech_name = response.Data.SMU_SCH_MECHANIC;
+        this.employee_no = response.Data.SMU_SCH_EMPCODE;
+        this.time_reported = response.Data.JOB_START_TIME;
+        this.time_left = response.Data.JOB_END_TIME;
+        this.type_of_bd = response.Data.SMU_SCH_BRKDOWNTYPE;
+        this.bd_status = this.storaged_data.breakdown_service;
+        this.areas_of_bd = 'Breakdown , Breakdown1 , Break Down 3';
+        this.remarks = this.storaged_data.feedback_remark_text
+        this.tech_signature = this.storaged_data.tech_signature;
+        this.customer_signature = this.storaged_data.customer_acknowledgemnet;
+        this.signature_date = this.storaged_data.date_of_submission;
+      });
+
+      let d = {
+        feedback_details: this.storaged_data.feedback_details
+      }
+      this._api.fetch_area_breakdown(d).subscribe((response: any) => {
+        console.log(response.Data);
+      });
+
+
+
     })
 
     let a = {
-      ISO_DRH_MODULE : 'BREAKDOWN',
-      ISO_DRH_LETYPE : 'L'
+      ISO_DRH_MODULE: 'BREAKDOWN',
+      ISO_DRH_LETYPE: 'L'
     }
     this._api.fetch_iso_number(a).subscribe((response: any) => {
       console.log(response.Data);
@@ -80,45 +110,12 @@ export class BreakdownservicePdfComponent implements OnInit {
     this._api.fetch_branch_address(datas).subscribe((response: any) => {
       console.log(response.Data);
       this.branch_name = response.Data[0].BRANCNAME;
-    })
-
-
-    this._api.fetch_breakdown_job_detail(datas).subscribe((response: any) => {
-      console.log(response.Data);
-
-      this.job_no = response.Data.SMU_SCH_JOBNO;
-      this.customer_name = response.Data.SMU_SCH_CUSNAME;
-      this.location = response.Data.SMU_SCH_CUSADD1 + "," + response.Data.SMU_SCH_CUSADD2 + "," + response.Data.SMU_SCH_CUSADD3 + "," + response.Data.SMU_SCH_CUSADD4 + "," + response.Data.SMU_SCH_CUSCODE;
-      this.date = response.Data.SMU_SCH_CRTDT;
-
-
-      this.nature_of_contract = response.Data.SMU_SCH_AMCTYPE;
-      this.breakdown_no = response.Data.SMU_SCH_COMPNO;
-      this.tech_name = response.Data.SMU_SCH_MECHANIC;
-      this.employee_no = response.Data.SMU_SCH_EMPCODE;
-      this.time_reported = response.Data.JOB_START_TIME;
-      this.time_left = response.Data.JOB_END_TIME;
-
-
-      this.type_of_bd = response.Data.SMU_SCH_BRKDOWNTYPE;
-      this.bd_status = this.storaged_data.breakdown_service;
-      this.areas_of_bd = 'Breakdown , Breakdown1 , Break Down 3';
-      this.remarks = this.storaged_data.feedback_remark_text
-
-      this.tech_signature = this.storaged_data.tech_signature;
-      this.customer_signature = this.storaged_data.customer_acknowledgemnet;
-      this.signature_date  = this.storaged_data.date_of_submission;
+    });
 
 
 
-      
-
-    })
 
 
-    
-
-    
 
 
   }
@@ -127,13 +124,13 @@ export class BreakdownservicePdfComponent implements OnInit {
 
 
   printComponent(cmpName) {
-     let printContents = document.getElementById(cmpName).innerHTML;
-     let originalContents = document.body.innerHTML;
-     document.body.innerHTML = printContents;
-     window.print();
-     document.body.innerHTML = originalContents;
-     window.location.reload();
-}
+    let printContents = document.getElementById(cmpName).innerHTML;
+    let originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+    window.location.reload();
+  }
 
 
 
